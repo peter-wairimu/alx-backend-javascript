082be9a00a02a52f5d0da0f0ea11dd41ba7c07cd
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-12-22 18:25:06 +0300

    Es6 classes

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+import Currency from "./3-currency";
+
+export default class Pricing {
+    constructor(amount, currency) {
+        this._amount = amount;
+        this._currency = currency;
+    }
+    
+    set amount(amount) {
+        this._amount = amount;
+    }
+    get amount() {
+        return this._amount;
+    }
+    set currency(currency) {
+        this._currency = currency;
+    }
+    get currency() {
+        return this._currency;
+    }
+    
+    displayFullPrice() {
+        return `${this._amount} ${this._currency.displayFullCurrency()}`;
+    }
+
+    static convertPrice(amount, conversionRate) {
+        return amount * conversionRate;
+    }
+
+    }

17b9d0071879e88afb4c25e412bb809a8e5c10a4
peter-wairimu <peter.wairimu@student.moringaschool.com>
2023-02-21 22:45:17 +0300

    node basics

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const port = 1245;
+
+// create a server object
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('Hello Holberton School!'); 
+  res.end(); 
+});
+app.listen(port);
+module.exports = app;

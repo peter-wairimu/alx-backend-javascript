3d4375ae5e9d96729540fa82b21e2ea34c098132
peter-wairimu <peter.wairimu@student.moringaschool.com>
2023-02-24 22:37:58 +0300

    Post integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,62 @@
+const chai = require('chai');
+const expect = chai.expect;
+const request = require('request')
+
+describe('API', function() {
+  describe('GET /', function() {
+    it('200, Welcome to the payment system', function(done) {
+      const object = {
+        url: 'http://127.0.0.1:7865',
+        method: 'GET',
+      };
+      request(object, function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+      });
+    });
+  });
+
+  describe('GET /cart/12', () => {
+    it('Responds with 200 and id 12 in msg', function(done) {
+      const options = {
+        url: 'http://localhost:7865/cart/12',
+        method: 'GET',
+      };
+
+      request(options, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal('Payment methods for cart 12');
+        done();
+      });
+    });
+  });
+
+  describe('GET /cart/hello', function() {
+    it('Responds with 404', (done) => {
+      const options = {
+        url: 'http://localhost:7865/cart/hello',
+        method: 'GET',
+      };
+
+      request(options, function (error, response, body) {
+        expect(response.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
+
+  describe('GET /cart/', function() {
+    it('Responds with 404', (done) => {
+      const options = {
+        url: 'http://localhost:7865/cart/',
+        method: 'GET',
+      };
+
+      request(options, function (error, response, body) {
+        expect(response.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
+});
